5dc45573c314e8bf2510506d555c5236273d3554
MnlPhlp <online@philipp-manuel.de>
2025-12-30 01:24:40 +0100

    .

--- src/main.rs
+++ src/main.rs
@@ -37,9 +37,23 @@ pub fn Hero() -> Element {
     }
 }
 
+fn create_config(frames: Vec<[[bool; 44]; 11]>) -> String {
+    let mut bitstring = String::new();
+    for y in 0..11 {
+        for frame in &frames {
+            for x in 0..44 {
+                bitstring.push(if frame[y][x] { 'X' } else { '_' });
+            }
+        }
+    }
+    bitstring
+}
+
 #[component]
 pub fn Editor() -> Element {
     let mut frames = use_signal(|| vec![[[false; 44]; 11]]);
+    let mut adding = use_signal(|| true);
+    let mut active = use_signal(|| false);
     rsx! {
         div {
             class: "flex flex-col gap-4",
@@ -51,14 +65,35 @@ pub fn Editor() -> Element {
                             div {
                                 class: "flex",
                                 for x in 0..44 {
-                                    div {
+                                    button {
                                         class: "w-4 h-4 border border-gray-300",
+                                        class: if frames.read()[frame_index][y][x] { " bg-white" } else { "bg-black" },
+                                        onmousedown: move |_| {
+                                            *adding.write() = !frames()[frame_index][y][x];
+                                            *active.write() = true;
+                                            frames.write()[frame_index][y][x] = adding();
+                                        },
+                                        onmouseenter: move |_| { if active() { frames.write()[frame_index][y][x] = adding() }},
+                                        onmouseup: move |_| { *active.write() = false }
                                     }
                                 }
 
                             }
                         }
                     },
+                    button {
+                        class: "p-2 bg-blue-500 text-white btn",
+                        onclick: {
+                            move |_| {
+                                for y in 0..11 {
+                                    for x in 0..44{
+                                        frames.write()[frame_index][y][x] = !frames()[frame_index][y][x];
+                                    }
+                                }
+                            }
+                        },
+                        "invert"
+                    }
                     button {
                         class: "p-2 bg-red-500 text-white btn",
                         onclick: {
@@ -70,13 +105,21 @@ pub fn Editor() -> Element {
                     }
                 }
 
-            }
+            },
             button {
                 class: "p-2 bg-blue-500 text-white btn",
                 onclick: move |_| {
                     frames.push([[false; 44]; 11]);
                 },
                 "Add Frame"
+            },
+            button {
+                onclick: move |_| {
+                    let file = create_config(frames());
+                    // trigger download
+
+                },
+                "Export"
             }
 
         }
