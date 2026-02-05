e4a29aecd96854ea1dae77f1fd6bf7f296310e3e
MnlPhlp <online@philipp-manuel.de>
2026-02-05 19:15:30 +0100

    wasm kind of works

--- build.rs
+++ build.rs
@@ -1,3 +1,5 @@
+use slint_build::CompilerConfiguration;
+
 fn main() {
-    slint_build::compile("ui/appwindow.slint").unwrap();
+    slint_build::compile_with_config("ui/appwindow.slint",CompilerConfiguration::default().with_style("fluent".to_string())).unwrap();
 }
